f5a7729856054f3a19fff6f8ff673dba02008c8e
Myo T Kyaw <myotkyaw@gmail.com>
2016-03-21 12:43:54 -0700

    Get form input and pass as variable

--- register.php
+++ register.php
@@ -7,11 +7,23 @@
         <em><a href="index.php">Home</a></em>
         <br /><br />
         <form action="register.php" method="POST">
-            Enter Username: <input type="text" name="username" required="required" />
+            <label>Enter Username:</label>
+            <input type="text" name="username" required="required" />
             <br />
-            Enter Password: <input type="text" name="password" required="required" />
+            <label>Enter Password:</label>
+            <input type="text" name="password" required="required" />
             <br />
             <input type="submit" value="Register">
         </form>
     </body>
 </html>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysql_real_escape_string($_POST["username"]);
+    $password = mysql_real_escape_string($_POST["password"]);
+
+    echo "Username entered was: " . $username . "<br />";
+    echo "Password entered was: " . $password;
+}
+?>
