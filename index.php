e7dad46767422eab9cb0d09e8cd00feaa1eaed1c
Myo T Kyaw <myotkyaw@gmail.com>
2016-03-21 11:20:26 -0700

    Initial repository

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>PHP CRUD App - Authentication</title>
+    </head>
+    <body>
+        <?php echo "<p>Welcome to PHP CRUD Authentication App</p>" ?>
+    </body>
+</html>
