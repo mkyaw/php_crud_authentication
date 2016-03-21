e0068db0e460f57f7c5aa5640da503cd88bcdf3b
Myo T Kyaw <myotkyaw@gmail.com>
2016-03-21 13:37:59 -0700

    Get form input and check against database values - check login

---
+++ checklogin.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    $username = mysql_real_escape_string($_POST["username"]);
+    $password = mysql_real_escape_string($_POST["password"]);
+
+    mysql_connect("localhost", "root", "root") or die(mysql_error());
+    mysql_select_db("php_crud_auth") or die("Cannot connect to database");
+    
+    $query = mysql_query("SELECT * FROM users WHERE username='$username'");
+    $exists = mysql_num_rows($query);
+    $table_users = "";
+    $table_passwords = "";
+    if($exists > 0) {
+        while($row = mysql_fetch_array($query)) {
+            $table_users = $row["username"];
+            $table_passwords = $row["password"];
+        }
+
+        if(($username == $table_users) && ($password == $table_passwords)) {
+            /** $username in session ==> global variable **/
+            $_SESSION['user'] = $username;
+            
+            /** redirect the user to user home page **/
+            // header("location: user_home.php");
+            
+            echo "<script>alert('Logged in successfully!');</script>";
+            echo "<script>window.location.assign('index.php');</script>";
+        } else {
+            echo "<script>alert('Incorrect Password');</script>";
+            echo "<script>window.location.assign('login.php');</script>";
+        }
+    
+    } else {
+        echo "<script>alert('Incorrect Username');</script>";
+        echo "<script>window.location.assign('login.php');</script>";
+    }
+
+?>
