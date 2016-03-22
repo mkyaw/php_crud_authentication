f66c6997918e8c49c962707db46b98db7a6766a8
Myo T Kyaw <myotkyaw@gmail.com>
2016-03-22 13:22:42 -0700

    Set up add.php to add list items to database

--- add.php
+++ add.php
@@ -1,14 +1,39 @@
 <?php
+
+    /***
+    strftime() - get's the time based on what format your placed.
+    %X - current system time.
+    %B - current system month.
+    %d - current system day.
+    %Y - current system year.
+    ***/
+    
     session_start();
     if($_SESSION["user"]) {
     } else {
         header("location:index.php");
     }
 
-    $details = mysql_real_escape_string($_POST["details"]);
-    $time = strftime("%X");
-    $date = strftime("%B %d, %Y");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $details = mysql_real_escape_string($_POST["details"]);
+        $time = strftime("%X");
+        $date = strftime("%B %d, %Y");
 
-    echo "$time - $date - $details";
+        $decision = "no";
+
+        mysql_connect("localhost", "root", "root") or die(mysql_error());
+        mysql_select_db("php_crud_auth") or die("Cannot select the database");
+
+        foreach($_POST['public'] as $each_check) {
+            if($each_check != null) {
+                $decision = "yes";
+            }
+        }
+
+        mysql_query("INSERT INTO list(details, date_posted, time_posted, public) VALUES('$details', '$date', '$time', '$decision')");
+        header("location: user_home.php");
+    } else {
+        header("location: user_home.php");
+    }
 
 ?>
